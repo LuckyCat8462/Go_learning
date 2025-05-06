1a6abaa30eb0c86b7b0afd0601a2c94e0be454e1
neko <guanxin.liu@foxmail.com>
2025-05-06 22:50:45 +0800

    consul注册

--- src/6-MicroServices/08-consul指令.go
+++ src/6-MicroServices/08-consul指令.go
@@ -1,1 +1,25 @@
 package main
+
+// consul常用指令
+// - consul agent
+//    - -bind=0.0.0.0 		指定 consul所在机器的 IP地址。 默认值：0.0.0.0
+//    - -http-port=8500    consul 自带一个web访问的默认端口：8500
+//    - -client=127.0.0.1   表明哪些机器可以访问consul 。 默认本机。0.0.0.0 所有机器均可访问。
+//    - -config-dir=foo      所有主动注册服务的 描述信息
+//    - -data-dir=path       储存所有注册过来的srv机器的详细信息。
+//    - -dev                         开发者模式，直接以默认配置启动 consul
+//    - -node=hostname  服务发现的名字。
+//    - -rejoin                     consul 启动的时候，加入到的 consul集群
+//    - -server                    以服务方式开启consul， 允许其他的consul 连接到开启的 consul上 （形成集群）。如果不加 -server， 表示以 “客户端” 的方式开启。不能被连接。
+//    - -ui 		                  可以使用 web 页面 来查看服务发现的详情
+
+// 小案例
+// 以server模式启动一个consul
+// 记得ip地址要换成自己的ip地址
+// consul agent -server -bootstrap-expect 1 -data-dir /tmp/consul -node=n1 -bind=192.168.6.108 -ui -rejoin -config-dir=/etc/consul.d/ -client 0.0.0.0
+// 可以看到提示consul agent running
+// 进入浏览器,输入192.168.6.108:8500,可以看到consul实例的页面
+
+// consul members	查看集群中有多少成员
+// consul info	查看当前consul的ip信息
+// consul leave	优雅的关闭consul
--- src/6-MicroServices/09-注册服务到consul.go
+++ src/6-MicroServices/09-注册服务到consul.go
@@ -1,1 +1,20 @@
 package main
+
+// 1.首先要cd进入到指定的目录下创建定义服务文件,来注册一个服务
+// 2.创建json文件,例如sudo vim web.json
+// 3.按照json的格式,填写服务信息
+// 服务定义文件在我们的配置目录下, /etc/consul.d/	,文件都是.json结尾
+// {"service":{
+// "name":"Faceid",
+// "tags":["rails"],
+// "port":9000}
+// }
+// 其中name是服务名,tags是别名,可以有多个,但是要用[]包裹,port是端口号
+
+// 4.重新启动consul
+// 5.查询服务
+// 5.1.浏览器查看
+// 5.2.指令查看
+// curl -s 192.168.81.128:8500/v1/catalog/service/nk86
+
+// 可以将生成的json文件复制到json.cn上在线解析成更方便观看的格式
