3248f2c5f7b21687bd02b51231a7a9a9d3e764b0
neko <guanxin.liu@foxmail.com>
2025-05-09 19:31:49 +0800

    更改文件夹

---
+++ src/6-MicroServicesBasics/06-grpc_server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"6-MicroServices/pb"
+	"context" // context上下文. --- goroutine (go程) 之间用来进行数据传递 API 包
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
+)
+
+// 定义类
+type Children struct {
+}
+
+// 按接口绑定类方法
+func (this *Children) SayHello(ctx context.Context, t *pb.Teacher) (*pb.Teacher, error) {
+	t.Name += " is Sleeping"
+	return t, nil
+}
+
+func main() {
+	// 1. 初始一个 grpc 对象
+	grpcServer := grpc.NewServer()
+
+	// 2. 注册服务
+	// alt+enter可以选择插入的包
+	pb.RegisterSayNameServer(grpcServer, new(Children))
+
+	// 3. 设置监听， 指定 IP、port
+	listener, err := net.Listen("tcp", "127.0.0.1:8800")
+	if err != nil {
+		fmt.Println("Listen err:", err)
+		return
+	}
+	defer listener.Close()
+
+	// 4. 启动服务。---- serve()
+	grpcServer.Serve(listener)
+}
--- src/7-MicroServices/06-consul结合grpc_grpc远程调用.go
+++ src/7-MicroServices/06-consul结合grpc_grpc远程调用.go
@@ -1,4 +1,4 @@
 package main
 
 // 1.创建pc包,并创建person.proto
-// 2.执行protoc --go_out=plugins=grpc:./*.proto
+// 2.执行protoc --go_out=plugins=grpc:./ *.proto
